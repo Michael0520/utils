be3001f0f1ba3a9fbaec65abe4400cfa8203bf46
Michael Lo <michael.lo@buyandship.com.tw>
2023-12-13 07:12:45 +0800

    feat: 🎸 update

---
+++ pages/navigation/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const online = useOnline()
+
+const { state, next, prev } = useCycleList([
+  'Dog',
+  'Cat',
+  'Lizard',
+  'Shark',
+  'Whale',
+  'Dolphin',
+  'Octopus',
+  'Seal',
+])
+
+definePageMeta({
+  layout: 'home',
+})
+
+</script>
+
+<template>
+  <div>
+    <Logos mb-6 />
+    <h1 class="text-primary text-unset text-4xl mb-6">vue/use : useCycleList</h1>
+    <Suspense>
+      <ClientOnly>
+        <div>
+          <div class="text-primary text-lg font-bold m-4">
+            State : {{ state }}
+          </div>
+          <div class="flex items-center justify-center gap-4">
+            <button class="btn" @click="prev()">
+              Prev
+            </button>
+            <button class="btn" @click="next()">
+              Next
+            </button>
+
+          </div>
+        </div>
+      </ClientOnly>
+    </Suspense>
+
+  </div>
+</template>
